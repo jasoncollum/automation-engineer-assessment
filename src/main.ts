c82c28cbecda96f289cbd71e445adf2c1e3c3d67
Mike Ulvila <mulvila@freshtechnology.com>
2024-06-05 12:15:26 -0500

    add validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 
-async function bootstrap() {
+async function bootstrap(): Promise<INestApplication> {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
+  return app;
 }
-bootstrap();
+
+bootstrap().then(async (app) => {
+  console.log(`Application is running on: ${await app.getUrl()}`);
+});
