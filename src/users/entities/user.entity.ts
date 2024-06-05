5d431bc27746df1ba3149e9cae823544dfeb9c19
Mike Ulvila <mulvila@freshtechnology.com>
2024-06-05 16:50:45 -0500

    add user crud logic

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,11 @@
-export class User {}
+export class User {
+  constructor(id: number, email: string, name: string) {
+    this.id = id;
+    this.email = email;
+    this.name = name;
+  }
+
+  id: number;
+  email: string;
+  name: string;
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
+  users: User[] = [];
+  userIdCounter = 1;
+
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    const id = this.userIdCounter++;
+    const user = new User(id, createUserDto.email, createUserDto.name);
+    this.users.push(user);
+    return user;
   }
 
   findAll() {
-    return `This action returns all users`;
+    return this.users;
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} user`;
+    const user = this.users.find((user) => user.id === id);
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return user;
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+    const user = this.findOne(id);
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    Object.assign(user, updateUserDto);
   }
 
   remove(id: number) {
-    return `This action removes a #${id} user`;
+    const index = this.users.findIndex((user) => user.id === id);
+    if (index === -1) {
+      throw new NotFoundException('User not found');
+    }
+    this.users.splice(index, 1);
   }
 }
